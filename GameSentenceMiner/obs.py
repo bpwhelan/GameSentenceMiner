f65aad07bf2338f4a16a5c50ae0692dafafb66bc
Beangate <bpwhelan95@gmail.com>
2025-04-18 17:44:17 -0400

    fix again

--- GameSentenceMiner/obs.py
+++ GameSentenceMiner/obs.py
@@ -73,8 +73,8 @@ def get_obs_websocket_config_values():
     if get_config().obs.password == 'your_password':
         logger.info("OBS WebSocket password is not set. Setting it now...")
         config = get_master_config()
-        config.obs.port = server_port
-        config.obs.password = server_password
+        config.get_config().port = server_port
+        config.get_config().password = server_password
         with open(get_config_path(), 'w') as file:
             json.dump(config.to_dict(), file, indent=4)
         reload_config()
